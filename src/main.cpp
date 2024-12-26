ecaa15f4666a866676968a0114a8d3a84a32e2a3
Gabe Ochoa <gabeochoa@meta.com>
2024-12-26 17:19:46 -0500

    add sign helper fn

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,10 @@ bool running = true;
 //
 using namespace afterhours;
 
+template <typename T> int sgn(T val) {
+    return (T(0) < val) - (val < T(0));
+}
+
 constexpr float distance_sq(const vec2 a, const vec2 b) {
   return (a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y);
 }
