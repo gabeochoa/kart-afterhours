6e3304714bf87feccf308f8aa8aef6b5876c4571
Gabe Ochoa <gabeochoa@meta.com>
2025-01-10 01:18:02 -0500

    format code

--- src/makers.cpp
+++ src/makers.cpp
@@ -79,41 +79,44 @@ void make_bullet(Entity &parent, const Weapon &wp, float angle_offset) {
     angle = 180.f;
     break;
   }
-  
+
   if (wp.config.spread > 0.f) {
-    std::mt19937_64 rng;    
-    const auto timeSeed = std::chrono::high_resolution_clock::now().time_since_epoch().count();
-    std::seed_seq ss{uint32_t(timeSeed & 0xffffffff), uint32_t(timeSeed>>32)};
+    std::mt19937_64 rng;
+    const auto timeSeed =
+        std::chrono::high_resolution_clock::now().time_since_epoch().count();
+    std::seed_seq ss{uint32_t(timeSeed & 0xffffffff), uint32_t(timeSeed >> 32)};
     rng.seed(ss);
-    std::uniform_real_distribution<float> unif(-wp.config.spread, wp.config.spread);
+    std::uniform_real_distribution<float> unif(-wp.config.spread,
+                                               wp.config.spread);
     angle_offset += wp.config.size.x * unif(rng);
   }
-  
+
   vec2 spawn_bias{0, wp.config.size.y};
   auto bullet_spawn_pos = transform.pos() + spawn_bias;
-  
+
   auto &bullet = EntityHelper::createEntity();
-  bullet
-      .addComponent<Transform>(bullet_spawn_pos,
-                               wp.config.size)
+  bullet.addComponent<Transform>(bullet_spawn_pos, wp.config.size)
       .set_angle(angle_offset);
 
   bullet.addComponent<CanDamage>(parent.id, wp.config.base_damage);
   bullet.addComponent<HasLifetime>(wp.config.life_time_seconds);
 
-  // If the config doesn't want wrapping, this will make the bullet go into the "void"
-  auto wrap_padding = wp.config.can_wrap_around ? 0.f : std::numeric_limits<float>::max();
+  // If the config doesn't want wrapping, this will make the bullet go into the
+  // "void"
+  auto wrap_padding =
+      wp.config.can_wrap_around ? 0.f : std::numeric_limits<float>::max();
   bullet.addComponent<CanWrapAround>(wrap_padding);
 
   bullet.addComponent<HasEntityIDBasedColor>(
       parent.id, parent.get<HasColor>().color, raylib::RED);
-      
+
   const auto rad = transform.as_rad() + to_radians(angle + angle_offset);
-  auto& bullet_transform = bullet.get<Transform>();
+  auto &bullet_transform = bullet.get<Transform>();
   bullet_transform.velocity =
       vec2{std::sin(rad) * wp.config.speed, -std::cos(rad) * wp.config.speed};
   bullet_transform.accel = wp.config.acceleration;
-  bullet_transform.render_out_of_bounds = wp.config.can_wrap_around && wp.config.render_out_of_bounds; 
+  bullet_transform.render_out_of_bounds =
+      wp.config.can_wrap_around && wp.config.render_out_of_bounds;
   bullet_transform.cleanup_out_of_bounds = !wp.config.can_wrap_around;
 }
 
@@ -132,17 +135,22 @@ Entity &make_car(int id) {
   entity.addComponent<TireMarkComponent>();
   auto tint = get_color_for_player((size_t)id);
   entity.addComponent<HasColor>(tint);
-  entity.addComponent<HasSprite>(idx_to_sprite_frame(0, 1), 1.f, tint);
+
+  const Transform &transform = entity.get<Transform>();
+  entity.addComponent<afterhours::texture_manager::HasSprite>(
+      transform.position, transform.size, transform.angle,
+      idx_to_sprite_frame(0, 1), 1.f, tint);
 
   entity.addComponent<CanShoot>()
       .register_weapon(InputAction::ShootLeft, Weapon::FiringDirection::Forward,
                        Weapon::Type::Shotgun)
-      .register_weapon(InputAction::ShootRight, Weapon::FiringDirection::Forward,
+      .register_weapon(InputAction::ShootRight,
+                       Weapon::FiringDirection::Forward,
                        Weapon::Type::MachineGun);
-      // .register_weapon(InputAction::ShootLeft, Weapon::FiringDirection::Left,
-      // Weapon::Type::Cannon)
-      //.register_weapon(InputAction::ShootRight, Weapon::FiringDirection::Right,
-      //                 Weapon::Type::Cannon);
+  // .register_weapon(InputAction::ShootLeft, Weapon::FiringDirection::Left,
+  // Weapon::Type::Cannon)
+  //.register_weapon(InputAction::ShootRight, Weapon::FiringDirection::Right,
+  //                 Weapon::Type::Cannon);
 
   return entity;
 }
@@ -150,25 +158,27 @@ Entity &make_car(int id) {
 void make_player(input::GamepadID id) {
   auto &entity = make_car(next_id++);
   entity.addComponent<PlayerID>(id);
-  
+
   const auto player_id_text = "[Player " + std::to_string(id) + "]";
   const auto player_label_pos_offset = vec2{-.1f, 0.f};
-  LabelInfo player_text_label_info(player_id_text, player_label_pos_offset, LabelInfo::LabelType::StaticText);
+  LabelInfo player_text_label_info(player_id_text, player_label_pos_offset,
+                                   LabelInfo::LabelType::StaticText);
 
   const auto velocity_unit_text = " m/s";
   const auto velocity_label_pos_offset = vec2{2.25f, 1.0f};
-  LabelInfo velocity_text_label_info(velocity_unit_text, velocity_label_pos_offset, LabelInfo::LabelType::VelocityText);
+  LabelInfo velocity_text_label_info(velocity_unit_text,
+                                     velocity_label_pos_offset,
+                                     LabelInfo::LabelType::VelocityText);
 
   const auto acceleration_unit_text = " m/s^2";
   const auto acceleration_label_pos_offset = vec2{2.25f, 2.0f};
-  LabelInfo acceleration_text_label_info(acceleration_unit_text, acceleration_label_pos_offset, LabelInfo::LabelType::AccelerationText);
+  LabelInfo acceleration_text_label_info(
+      acceleration_unit_text, acceleration_label_pos_offset,
+      LabelInfo::LabelType::AccelerationText);
 
-  std::vector<LabelInfo> player_labels
-  {
-    std::move(player_text_label_info),
-    std::move(velocity_text_label_info),
-    std::move(acceleration_text_label_info)
-  };
+  std::vector<LabelInfo> player_labels{std::move(player_text_label_info),
+                                       std::move(velocity_text_label_info),
+                                       std::move(acceleration_text_label_info)};
 
   entity.addComponent<HasLabels>(std::move(player_labels));
 }
