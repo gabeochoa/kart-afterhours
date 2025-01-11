f270c69414a44e706c13ab4abe206195d25052a6
Gabe Ochoa <gabeochoa@meta.com>
2025-01-11 05:26:08 -0500

    bf? - sprite color now updates every frame :)

--- src/systems.h
+++ src/systems.h
@@ -8,11 +8,15 @@
 struct UpdateSpriteTransform
     : System<Transform, afterhours::texture_manager::HasSprite> {
 
-  virtual void for_each_with(Entity &, Transform &transform,
+  virtual void for_each_with(Entity &entity, Transform &transform,
                              afterhours::texture_manager::HasSprite &hasSprite,
                              float) override {
     hasSprite.update_transform(transform.position, transform.size,
                                transform.angle);
+
+    if (entity.has_child_of<HasColor>()) {
+      hasSprite.update_color(entity.get_with_child<HasColor>().color());
+    }
   }
 };
 
