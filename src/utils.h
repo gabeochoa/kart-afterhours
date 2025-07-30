d00045766d46f9f1acfc94ce0aedb518c843e03f
Gabe Ochoa <gabeochoa@meta.com>
2025-07-29 21:27:03 -0400

    be - merge sgn function from utils.h into math_util.h

--- src/utils.h
+++ src/utils.h
@@ -11,11 +11,6 @@ template <class... Ts> struct overloaded : Ts... {
   using Ts::operator()...;
 };
 
-// TODO merge with math_util.h
-template <typename T> int sgn(const T &val) {
-  return (T(0) < val) - (val < T(0));
-}
-
 std::string to_string(float value, int precision) {
   std::ostringstream stream;
   stream << std::fixed << std::setprecision(precision) << value;
